7ce8890f3dcff87e4d69e4861f85ab0eeec0ace6
Toril <hokseggentoril@hotmail.com>
2024-06-05 23:29:24 +0200

    fixed reduced motion

--- CA 2/poster.js
+++ CA 2/poster.js
@@ -1,13 +1,18 @@
-const rotateAnimation = document.getElementById('rotateAnimation');
+document.addEventListener('DOMContentLoaded', () => {
+    const rotateAnimationBig = document.getElementById('rotateAnimation');
 
 if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
     rotateAnimation.setAttribute('to', '45 1553.47 443.48'); //slight movement
     rotateAnimation.setAttribute('dur', '20s'); // slower rotation for reduced motion
 }
+});
 
-const rotateAnimationBig = document.getElementById('rotateAnimationBig');
+document.addEventListener('DOMContentLoaded', () => {
+    const rotateAnimationBig = document.getElementById('rotateAnimationBig');
 
-if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-    rotateAnimationBig.setAttribute('to', '45 1456.2 270.15'); //slight movement
-    rotateAnimationBig.setAttribute('dur', '30s'); // slower rotation for reduced motion
-}
+    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+        // Reduced motion preference detected
+        rotateAnimationBig.setAttribute('to', '45 1456.2 270.15');
+        rotateAnimationBig.setAttribute('dur', '10s'); // slower rotation for reduced motion
+    }
+});
